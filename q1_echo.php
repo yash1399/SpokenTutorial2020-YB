52d4780917052eb305e930819471e88407e9d9f2
Yash <bavaskaryash1399@gmail.com>
2020-09-29 20:59:29 +0530

    question 1 complete"

--- q1_echo.php
+++ q1_echo.php
@@ -2,14 +2,29 @@
     function evenodd ($number){
 
         if ($number%2 == 0){
-            echo "The number $number is an even number ";
+            echo "The number $number is an even number <br> ";
         }else{
-            echo "The number $number is an odd number"; 
+            echo "The number $number is an odd number <br>"; 
         }
 
+    };
+    evenodd(11);
+
+
+    function consvowel($alpha){
+
+        if( $alpha == 'a' || $alpha == 'e' || $alpha == 'i' || $alpha == 'o'|| $alpha == 'u'){
+            echo "The letter is $alpha is a vowel <br>";
+        }else{
+            echo "The letter $alpha is a consonant <br>";
+        }
+        
     }
 
-    evenodd(11)
+    consvowel("b")
+
+
+
 
 
 
