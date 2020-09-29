e2ecf2724f3ddf511a4a68ad404e1a449d67c618
Yash <bavaskaryash1399@gmail.com>
2020-09-29 20:52:36 +0530

    first commit

---
+++ q1_echo.php
@@ -0,0 +1,16 @@
+<?php
+    function evenodd ($number){
+
+        if ($number%2 == 0){
+            echo "The number $number is an even number ";
+        }else{
+            echo "The number $number is an odd number"; 
+        }
+
+    }
+
+    evenodd(11)
+
+
+
+?>
