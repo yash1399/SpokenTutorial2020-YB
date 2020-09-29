9fe3078f18caa9bc676ac2a7d1c847940872ad89
Yash <bavaskaryash1399@gmail.com>
2020-09-29 21:09:45 +0530

    corrected

--- A1/q1_echo.php
+++ A1/q1_echo.php
@@ -13,15 +13,30 @@
 
     function consvowel($alpha){
 
-        if( $alpha == 'a' || $alpha == 'e' || $alpha == 'i' || $alpha == 'o'|| $alpha == 'u'){
-            echo "The letter is $alpha is a vowel <br>";
-        }else{
-            echo "The letter $alpha is a consonant <br>";
-        }
+        switch ($alpha){
+            case "a" || "A":
+                echo "This letter $alpha is a <b>Vowel</b>";
+            break;
+            case "e" || "E" :
+                echo "This letter  $alpha is a <b>Vowel</b>";
+            break;
+            case "i" || "I":
+                echo "This letter  $alpha is a <b>Vowel</b>";
+            break;
+            case "o" || "O": 
+                echo "This letter $alpha is a <b>Vowel</b>";
+            break;
+            case "u" || "U":
+                echo "This letter $alpha is a <b>Vowel</b>";
+            break;
+
+            default: 
+            echo "This letter $alpha is a <b>Consonant</b>";
         
+        }
     }
 
-    consvowel("b")
+    consvowel("I")
 
 
 ?>
